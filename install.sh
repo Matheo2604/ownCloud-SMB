cc8442d6a7b005307bce29b5deb73a1c2905303f
Matheo2604 <159913566+Matheo2604@users.noreply.github.com>
2026-04-16 13:22:13 +0200

    Update

--- install.sh
+++ install.sh
@@ -31,15 +31,14 @@ apt-get -y install apt-transport-https lsb-release ca-certificates wget
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
 apt-get update
-apt-get -y install php7.4-{xml,intl,common,json,curl,mbstring,mysql,gd,imagick,zip,opcache} libapache2-mod-php7.4 php7.4
+apt-get -y install php7.4-{xml,intl,common,json,curl,mbstring,mysql,gd,imagick,zip,opcache,redis,apcu} libapache2-mod-php7.4 php7.4 redis
 
 
 # Add ownCloud repository and install ownCloud
-echo 'deb https://download.opensuse.org/repositories/isv:/ownCloud:/server:/10.14.0/Debian_12/ /' > /etc/apt/sources.list.d/isv:ownCloud:server:10.list
-curl -fsSL https://download.opensuse.org/repositories/isv:/ownCloud:/server:/10.14.0/Debian_12/Release.key | gpg --dearmor > /etc/apt/trusted.gpg.d/isv_ownCloud_server_10.gpg
-apt update
-apt-get -y install owncloud-complete-files
-
+cd /var/www/
+wget https://download.owncloud.com/server/stable/owncloud-complete-latest.tar.bz2 && \
+tar -xjf owncloud-complete-latest.tar.bz2 && \
+chown -R www-data. owncloud
 
 # Configure Apache
 cat > /etc/apache2/sites-available/owncloud.conf << 'EOL'
@@ -56,6 +55,10 @@ cat > /etc/apache2/sites-available/owncloud.conf << 'EOL'
 
 	ErrorLog ${APACHE_LOG_DIR}/owncloud_error.log
 	CustomLog ${APACHE_LOG_DIR}/owncloud_access.log combined
+
+      <IfModule mod_headers.c>
+             Header always set Strict-Transport-Security "max-age=15552000; includeSubDomains"
+      </IfModule>
 
 	SSLEngine on
 	SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt
@@ -89,6 +92,7 @@ EOT
 
 # Restart apache to take in charge the new configuration
 a2enmod ssl
+a2enmod headers
 a2ensite owncloud.conf
 a2dissite 000-default.conf
 a2enmod rewrite mime unique_id
@@ -111,9 +115,39 @@ sudo -u www-data php occ maintenance:install \
    --database-name "ownclouddb" \
    --database-user "root"\
    --database-pass "$owncloud_password" \
+   --data-dir "/var/www/owncloud/data" \
    --admin-user "admin" \
    --admin-pass "$owncloud_password"
-sed -i "/0 => 'localhost',/a\	    1 => '$domain'," /var/www/owncloud/config/config.php
+
+
+# Setup occ in the binairie folder
+FILE="/usr/local/bin/occ"
+cat <<EOM >$FILE
+#! /bin/bash
+cd /var/www/owncloud
+sudo -E -u www-data /usr/bin/php /var/www/owncloud/occ "\$@"
+EOM
+chmod +x $FILE
+
+
+# Setup the trusted domain given by the user
+occ config:system:set trusted_domains 1 --value="$domain"
+
+
+# Setup memory caching to owncloud
+occ config:system:set \
+   memcache.local \
+   --value '\OC\Memcache\APCu'
+occ config:system:set \
+   memcache.distributed\
+   --value '\OC\Memcache\Redis'
+occ config:system:set \
+   memcache.locking \
+   --value '\OC\Memcache\Redis'
+occ config:system:set \
+   redis \
+   --value '{"host": "/var/run/redis/redis.sock", "port": "0"}' \
+   --type json
 
 
 # Setup of basic Firewall rules
@@ -122,15 +156,34 @@ systemctl enable --now ufw
 ufw allow "Apache Full"
 
 
+# Setup basic cron task
+echo "*/15  *  *  *  * /var/www/owncloud/occ system:cron" \
+  | sudo -u www-data -g crontab tee -a \
+  /var/spool/cron/crontabs/www-data
+echo "0  2  *  *  * /var/www/owncloud/occ dav:cleanup-chunks" \
+  | sudo -u www-data -g crontab tee -a \
+  /var/spool/cron/crontabs/www-data
+
+
+# Passing redis(mod for memory caching) from tcp to sock
+/usr/sbin/usermod -G redis -a www-data
+mkdir -p /var/run/redis/
+chown -R redis:www-data /var/run/redis
+sudo sed -i 's/^\s*port\s\+.*/port 0/' /etc/redis/redis.conf
+sudo sed -i 's|^\s*#\?\s*unixsocket\s\+.*|unixsocket /var/run/redis/redis.sock|' /etc/redis/redis.conf
+sudo sed -i 's/^\s*#\?\s*unixsocketperm\s\+.*/unixsocketperm 770/' /etc/redis/redis.conf
+systemctl restart redis
+
+
 # Restart apache take in count the new configuration 
 systemctl restart apache2
 
 
 # Reminder of the username, password and domain
-echo ''
-echo "The installation is now finish. Enjoy !"
-echo "You can now connect to https://$domain"
-echo "username: admin"
-echo "password: $owncloud_password"
+occ -V
+echo "Your ownCloud is accessable under: admin"
+echo "Your ownCloud is accessable under: "$owncloud_password
+echo "Your ownCloud is accessable under: "$domain
+echo "The Installation is complete."
 echo ''
 exit 0
